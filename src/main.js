27b62bbf8fa800a5c960426a333954bee77a1432
Ablyakimov <unted575@gmail.com>
2023-05-08 17:34:34 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,64 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import config from 'config'
+
+import { ogg } from './ogg.js'
+import { openai } from './openai.js'
+
+const INITIAL_SESSION = {
+  messages: []
+}
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
+
+bot.use(session())
+
+bot.command('new', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
+bot.command('start', async (ctx) => {
+  ctx.session = INITIAL_SESSION
+  await ctx.reply('Жду вашего голосового или текстового сообщения')
+})
+
+bot.on(message('voice'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+
+    const oggPath = await ogg.create(link.href, userId)
+    const mp3Path = await ogg.toMp3(oggPath, userId)
+
+    const text = await openai.transcription(mp3Path)
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: text })
+    const responce = await openai.chat(ctx.session.messages)
+    ctx.session.messages.push({ role: openai.roles.ASISTANT, content: responce.content })
+    
+    await ctx.reply(responce.content)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+bot.on(message('text'), async (ctx) => {
+  ctx.session ??= INITIAL_SESSION
+  try {
+
+    ctx.session.messages.push({ role: openai.roles.USER, content: ctx.message.text })
+    const responce = await openai.chat(ctx.session.messages)
+    ctx.session.messages.push({ role: openai.roles.ASISTANT, content: responce.content })
+    
+    await ctx.reply(responce.content)
+  } catch (error) {
+    console.log(error)
+  }
+})
+  
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
